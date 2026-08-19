f14e4b41ed31e9649fe6857575e78272117b416d
habibmrizki <habibboboho@gmail.com>
2026-08-19 21:49:24 +0700

    feat(router/router): add router

---
+++ src/router/Router.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route, Navigate } from "react-router";
+
+import Login from "../pages/Login/Login";
+import Profile from "../pages/Profile/Profile";
+import Layout from "../layouts/Layout";
+
+export default function MainRoutes() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+    </Routes>
+  );
+}

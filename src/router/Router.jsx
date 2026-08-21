a04b62b5db48bc0dcf83740080e35f5df0a8dea4
habibmrizki <habibboboho@gmail.com>
2026-08-21 19:20:09 +0700

    feat(router): add route for form page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router";
 import Login from "../pages/Login/Login";
 import Profile from "../pages/Profile/Profile";
 import Layout from "../layouts/Layout";
+import Form from "../pages/Form/Form";
 
 export default function MainRoutes() {
   return (
@@ -11,6 +12,7 @@ export default function MainRoutes() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/form" element={<Form />} />
       </Route>
     </Routes>
   );

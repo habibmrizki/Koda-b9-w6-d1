7774acba19dbddac7f7c82ae3535cd4cb86b4987
habibmrizki <habibboboho@gmail.com>
2026-08-21 19:19:08 +0700

    feat(pages): add Form page combining SurveyForm and SurveyList

---
+++ src/pages/Form/Form.jsx
@@ -0,0 +1,13 @@
+import SurveyForm from "../../components/SurverForm";
+import SurverList from "../../components/SurverList";
+
+function Form() {
+  return (
+    <div>
+      <SurveyForm />
+      <SurverList />
+    </div>
+  );
+}
+
+export default Form;

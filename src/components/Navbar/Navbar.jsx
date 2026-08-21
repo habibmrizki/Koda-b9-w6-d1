e2952450152992ec5d2f4f2e92374d21a639b601
habibmrizki <habibboboho@gmail.com>
2026-08-21 19:18:49 +0700

    feat(nav): add form link for authenticated users

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,6 +28,9 @@ const Navbar = () => {
           ) : (
             <>
               <li className="py-2 px-4 hover:bg-gray-100 rounded-lg">
+                <Link to="/form">Form</Link>
+              </li>
+              <li className="py-2 px-4 hover:bg-gray-100 rounded-lg">
                 <Link to="/profile">Profile</Link>
               </li>
               <li className="py-2 px-4 hover:opacity-80 rounded-lg bg-black text-white">

0fa63808e4db6a53859011c0ad4f78e787a49da0
habibmrizki <habibboboho@gmail.com>
2026-08-22 16:13:09 +0700

    feat: add todo route to MainRoutes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,6 +4,7 @@ import Login from "../pages/Login/Login";
 import Profile from "../pages/Profile/Profile";
 import Layout from "../layouts/Layout";
 import Form from "../pages/Form/Form";
+import Todo from "../pages/todo/Todo";
 
 export default function MainRoutes() {
   return (
@@ -13,6 +14,7 @@ export default function MainRoutes() {
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/form" element={<Form />} />
+        <Route path="/todo" element={<Todo />} />
       </Route>
     </Routes>
   );

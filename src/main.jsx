52893007786d0cf056a1a0e3c7b52b546a34215a
habibmrizki <habibboboho@gmail.com>
2026-08-22 16:06:45 +0700

    feat: setup persistStore and PersistGate for redux state

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,23 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import MainRoutes from "./router/Router";
 import AuthProvider from "./context/AuthProvider.jsx";
-import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
+// import App from "./App.jsx";
 
+import store, { persistor } from "./redux/store.js";
+import "./index.css";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <AuthProvider>
-          <MainRoutes />
-        </AuthProvider>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <AuthProvider>
+            <MainRoutes />
+          </AuthProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 );
